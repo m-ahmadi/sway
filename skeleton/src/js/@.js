1abcb62e6be605eec591415a173b89390f0b85dc
mohammad-ahmadi <mohammad.ahmad1989@yahoo.com>
2017-04-23 02:46:32 +0430

    skel

--- skeleton/src/js/@.js
+++ skeleton/src/js/@.js
@@ -1,1 +1,15 @@
-//
+define([
+	"core/util",
+	"core/pubsub"
+], (u, newPubSub) => {
+	const inst = u.extend( newPubSub() );
+	
+	function init() {
+		
+	}
+	
+	inst.beforeReady = beforeReady;
+	inst.init = init;
+	
+	return inst;
+});
